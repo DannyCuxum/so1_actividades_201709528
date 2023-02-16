83c7e1add344575ef20a8d7856e6537a1ebf6122
danny <3000347170101@ingenieria.usac.edu.gt>
2023-02-16 10:49:47 -0600

    Actividada 2

---
+++ Actividad2/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# chmod +x nombre_del_archivo.sh
+
+# Leer la variable
+github_user="DannyCuxum"
+
+# Hacer la consulta a la API de GitHub y guardar la respuesta en una variable
+response=$(curl -s https://api.github.com/users/$github_user)
+
+# Extraer los valores necesarios del JSON usando jq y guardarlos en variables
+id=$(echo $response | jq -r '.id')
+created_at=$(echo $response | jq -r '.created_at')
+
+# Imprimir el mensaje con los valores extraídos
+echo "hola $github_user. User ID: $id. cuenta fue creada el: $created_at."
+
+# Obtener la fecha actual en el formato deseado (YYYY-MM-DD)
+fecha=$(date +%Y-%m-%d)
+
+# Crear el directorio de registro si aún no existe
+if [ ! -d "/tmp/$fecha" ]; then
+    mkdir "/tmp/$fecha"
+fi
+
+# Crear el archivo de registro con el mensaje anterior
+echo "Hola $github_user. User ID: $id. Cuenta fue creada el: $created_at." >> "/tmp/$fecha/saludos.log"
